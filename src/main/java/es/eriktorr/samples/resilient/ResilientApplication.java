91b5ea58cf725d61eceefaf463fb6c9b5c0fb800
Erik Torres <erik.torres@codurance.com>
2018-10-02 15:49:16 +0200

    Prepare application template with Spring Boot

---
+++ src/main/java/es/eriktorr/samples/resilient/ResilientApplication.java
@@ -0,0 +1,13 @@
+package es.eriktorr.samples.resilient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResilientApplication.class, args);
+    }
+
+}
